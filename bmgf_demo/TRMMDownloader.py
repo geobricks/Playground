b8894054abf136b3cabee5d542beb4def24e06b9
guidofenix <guido.barbaglia@fao.org>
2014-09-08 16:34:50 +0200

    Download TRMM

--- bmgf_demo/TRMMDownloader.py
+++ bmgf_demo/TRMMDownloader.py
@@ -5,25 +5,13 @@ from pgeo.thread.bulk_download_threads_manager import BulkDownloadManager
 log = log.logger(__name__)
 
 year = '2000'
-month = '03'
+month = '02'
 from_day = 1
-to_day   = 31
+to_day = 28
 days = map(lambda x: str(x) if x > 9 else '0'+str(x), range(int(from_day), 1+int(to_day)))
-# log.info(days)
 
-# log.info('Get file-names for ' + year + '/' + month)
-
-# for day in days:
-
+tab_id = 'tab_0'
 file_list = t.list_layers_subset(year, month, from_day, to_day)
-
-filesystem_structure = {
-    'product': '3B42',
-    'year': year,
-    'month': month,
-    'day': str(from_day) + '-' + str(to_day)
-}
-
 bulk_download_objects = []
 for day in days:
     bdo = {
@@ -39,6 +27,5 @@ for day in days:
     }
     bulk_download_objects.append(bdo)
 
-tab_id = 'tab_0'
 mgr = BulkDownloadManager('trmm2', None, bulk_download_objects, tab_id)
 target_dir = mgr.run()

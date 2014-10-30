0a8c612c5929aea9ea1cf9b689fe752901f6f3e0
Kalimaha <guido.barbaglia@gmail.com>
2014-10-30 11:43:15 +0100

    ISO2 and ISO3 codes added.

--- playground/csv2json/converter.py
+++ playground/csv2json/converter.py
@@ -6,33 +6,36 @@ def convert():
 
     with open('MODIS_GAUL.csv', 'rb') as csvfile:
 
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        spam_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         current = '102'
         out = []
 
-        buffer = []
+        gaul_buffer = []
         buffers = {}
-        for row in spamreader:
+        for row in spam_reader:
             if row[0] == current:
-                buffer.append(row)
+                gaul_buffer.append(row)
             else:
                 current = row[0]
-                buffer = []
-                buffer.append(row)
-            buffers[row[1]] = buffer
+                gaul_buffer = [row]
+            buffers[row[1]] = gaul_buffer
 
         for key in buffers:
             hs = []
             vs = []
             gaul_code = None
-            for buffer in buffers[key]:
-                hs.append(int(buffer[3]))
-                vs.append(int(buffer[4]))
-                gaul_code = buffer[0]
+            for gaul_buffer in buffers[key]:
+                hs.append(int(gaul_buffer[3]))
+                vs.append(int(gaul_buffer[4]))
+                gaul_code = gaul_buffer[0]
+                iso2_code = gaul_buffer[8] if len(gaul_buffer[8]) > 0 else None
+                iso3_code = gaul_buffer[9] if len(gaul_buffer[9]) > 0 else None
             tmp = {
                 "to_v": max(vs),
                 "gaul_label": key,
                 "gaul_code": gaul_code,
+                "iso2_code": iso2_code,
+                "iso3_code": iso3_code,
                 "from_v": min(vs),
                 "from_h": min(hs),
                 "to_h": max(hs)
